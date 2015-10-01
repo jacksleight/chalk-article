c1336395677e13daa44d86b9907b486521854c80
Jack Sleight <jacksleight@gmail.com>
2015-10-01 20:32:51 +0100

    Initial commit.

---
+++ lib/Article.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Copyright 2015 Jack Sleight <http://jacksleight.com/>
+ * This source file is subject to the MIT license that is bundled with this package in the file LICENCE.md. 
+ */
+
+namespace Chalk\Article;
+
+use Chalk\Backend;
+use Chalk\Chalk;
+use Chalk\Event;
+use Chalk\InfoList;
+use Chalk\Module;
+use Closure;
+use Coast\Request;
+use Coast\Response;
+use Coast\Router;
+
+class Article extends Module
+{   
+    const VERSION = '0.5.0';
+
+    public function frontendInit()
+    {
+        
+    }
+    
+    public function backendInit()
+    {
+        
+    }
+
+    public function core_frontendInitNode($node, $content, $info, $params)
+    {
+        
+    }
+}
