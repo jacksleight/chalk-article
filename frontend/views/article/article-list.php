b86188ea13e960795d0d6f44da034fc0c09416dc
Jack Sleight <jacksleight@gmail.com>
2017-06-28 12:05:00 +0100

    Move pagination to own view

--- frontend/views/article/article-list.php
+++ frontend/views/article/article-list.php
@@ -3,11 +3,4 @@
         'article' => $article,
     ]) ?>
 <?php } ?>
-<ul>
-    <?php if ($pagination->page > 1) { ?>
-        <li><a href="<?= $this->url->query(['page' => $pagination->page - 1]) ?>">Previous</a></li>
-    <?php } ?>
-    <?php if ($pagination->page < $pagination->pages) { ?>
-        <li><a href="<?= $this->url->query(['page' => $pagination->page + 1]) ?>">Next</a></li>
-    <?php } ?>
-</ul>
+<?= $this->partial('pagination') ?>
